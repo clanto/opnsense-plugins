3b0526592eb1fe220a31ee3b1bbea8495eb55b41
clanto007 <44651109+clanto007@users.noreply.github.com>
2023-06-25 23:21:08 +0200

    Update convert.php

--- security/maltrail/src/opnsense/scripts/OPNsense/Maltrail/convert.php
+++ security/maltrail/src/opnsense/scripts/OPNsense/Maltrail/convert.php
@@ -8,9 +8,9 @@ use OPNsense\Core\Config;
 
 // traverse Maltrail old general to new sensor and server sections
 $configObj = Config::getInstance()->object();
-$configObj->OPNsense->Maltrail->general->heuristics = (string)$configObj->OPNsense->Maltrail->sensor->heuristics;
-$configObj->OPNsense->Maltrail->general->checkhostheader = (string)$configObj->OPNsense->Maltrail->sensor->checkhostheader;
-$configObj->OPNsense->Maltrail->general->updateperiod = (string)$configObj->OPNsense->Maltrail->sensor->updateperiod;
-$configObj->OPNsense->Maltrail->general->adminpassword = (string)$configObj->OPNsense->Maltrail->server->adminpassword;
-$configObj->OPNsense->Maltrail->general->monitorinterface = (string)$configObj->OPNsense->Maltrail->sensor->monitorinterface;
-$configObj->OPNsense->Maltrail->general->whitelist = (string)$configObj->OPNsense->Maltrail->sensor->whitelist;
+$configObj->OPNsense->Maltrail->sensor->heuristics = (string)$configObj->OPNsense->Maltrail->general->heuristics;
+$configObj->OPNsense->Maltrail->sensor->checkhostheader = (string)$configObj->OPNsense->Maltrail->general->checkhostheader;
+$configObj->OPNsense->Maltrail->sensor->updateperiod = (string)$configObj->OPNsense->Maltrail->general->updateperiod;
+$configObj->OPNsense->Maltrail->server->adminpassword = (string)$configObj->OPNsense->Maltrail->general->adminpassword;
+$configObj->OPNsense->Maltrail->sensor->monitorinterface = (string)$configObj->OPNsense->Maltrail->general->monitorinterface;
+$configObj->OPNsense->Maltrail->sensor->whitelist = (string)$configObj->OPNsense->Maltrail->general->whitelist;
